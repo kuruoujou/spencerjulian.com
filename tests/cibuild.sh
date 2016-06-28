d0fc593b08e21c7ac6ebba42417f104eb6531d48
Spencer Julian <helloThere@spencerjulian.com>
2016-06-27 21:24:12 -0400

    Add new test

---
+++ tests/cibuild.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This is ripped straight from the jekyll docs. Will hopefully modify a bit later.
+
+set -e # halt script on error
+
+bundle exec jekyll build
+bundle exec htmlproofer ./_site
