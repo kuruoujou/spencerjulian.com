f1690a7742b154ed737afb6c0ab9d6ad87306b4a
Spencer Julian <helloThere@spencerjulian.com>
2016-07-08 19:52:37 -0400

    Since travis doesn't seem capable of deploying to s3 on it's own, let's try doing it through a script instead.

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -5,6 +5,7 @@
 if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]); then
     echo "Build successful. Invalidating Cloudfront..."
     aws --version
+    aws s3 sync --region $AWS_REGION --acl public-read --delete _site/ s3://$AWS_S3_BUCKET
     echo -e "\n[preview]\ncloudfront = true" >> ~/.aws/config
     cat ~/.aws/config
     aws cloudfront create-invalidation --cli-input-json '{"DistributionId":"'"$AWS_CLOUDFRONT_ID"'","InvalidationBatch":{"Paths":{"Quantity":2,"Items":["/index.html","/*"]},"CallerReference":"'$(date "+%s")'"}}'
