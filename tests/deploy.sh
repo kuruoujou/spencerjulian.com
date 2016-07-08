26fcbcd2c075a9937df13680331b6609196090b0
Spencer Julian <helloThere@spencerjulian.com>
2016-07-08 19:05:30 -0400

    Update travis and deploy.sh to deploy to S3 and use Cloudfront.

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -3,8 +3,11 @@
 # This script will perform deployment steps specified in the "if" statement.
 
 if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]); then
-    echo "Build successful. De"
-    echo "No publish steps yet available. Publish was successful."
+    echo "Build successful. Invalidating Cloudfront..."
+    aws --version
+    echo -e "\n[preview]\ncloudfront = true" >> ~/.aws/config
+    cat ~/.aws/config
+    aws cloudfront create-invalidation --cli-input-json '{"DistributionId":"'"$AWS_CLOUDFRONT_ID"'","InvalidationBatch":{"Paths":{"Quantity":2,"Items":["/index.html","/*"]},"CallerReference":"'$(date "+%s")'"}}'
 else
     echo "Build successful. Pull requests and non-master branch do not publish."
 fi
