bb0234745d7dfc8d513f15db8cbf329de1ec8340
Spencer Julian <helloThere@spencerjulian.com>
2016-07-08 20:05:01 -0400

    update the deploy script to add some more debugging

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -3,11 +3,13 @@
 # This script will perform deployment steps specified in the "if" statement.
 
 if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]); then
-    echo "Build successful. Invalidating Cloudfront..."
+    echo "Build successful. Pushing to AWS..."
     aws --version
+    echo "Attempting to push _site to S3 bucket ${AWS_S3_BUCKET} in region ${AWS_REGION}"
     aws s3 sync --region $AWS_REGION --acl public-read --delete _site/ s3://$AWS_S3_BUCKET
-    echo -e "\n[preview]\ncloudfront = true" >> ~/.aws/config
-    cat ~/.aws/config
+    echo "Setting AWS cloudfront preview"
+    aws configure set preview.cloudfront true
+    echo "Invalidating cloudfront cache."
     aws cloudfront create-invalidation --cli-input-json '{"DistributionId":"'"$AWS_CLOUDFRONT_ID"'","InvalidationBatch":{"Paths":{"Quantity":2,"Items":["/index.html","/*"]},"CallerReference":"'$(date "+%s")'"}}'
 else
     echo "Build successful. Pull requests and non-master branch do not publish."
