67ad1895cb95242447006e56975fa7b84f40664e
Spencer Julian <helloThere@spencerjulian.com>
2016-07-08 20:29:26 -0400

    Maybe the error is incorrect?

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -7,6 +7,8 @@ if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]); then
     aws --version
     echo "Make sure credentials are valid."
     aws iam get-user
+    echo "Make sure _site directory exists and has files."
+    ls _site
     echo "Attempting to push _site to S3 bucket ${AWS_S3_BUCKET} in region ${AWS_REGION}"
     aws s3 sync --region $AWS_REGION --acl public-read --delete _site/ s3://$AWS_S3_BUCKET
     echo "Setting AWS cloudfront preview"
